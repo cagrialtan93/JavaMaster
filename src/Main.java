0955596fc148e6d28b6fd922908ca3cd1b3dd372
cagrialtan93 <cagrialtanhd@gmail.com>
2024-07-29 01:03:33 +0300

    coding stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Çağrı");
-	System.out.println("Hello world!");
+        System.out.println(toMilesPerHour(10.25));
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        double mile = kilometersPerHour / 1.609;
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            long roundedValue = Math.round(mile);
+            return roundedValue;
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + Math.round(kilometersPerHour / 1.609) + " mi/h");
+        }
     }
 }
