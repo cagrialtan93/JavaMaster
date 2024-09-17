883af49784bb15d5531f0059198e4fd2b4b44a16
Çağrı Altan <cagrialtanhd@gmail.com>
2024-09-17 15:10:39 +0300

    room challenge

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,20 @@ import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
-        SmartKitchen smartKitchen = new SmartKitchen();
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
 
-        smartKitchen.addWater();
-        smartKitchen.getBrewMaster().brewCoffee();
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedRoom = new Bedroom("YOUR NAME HERE", wall1, wall2, wall3, wall4, ceiling,bed, lamp);
+        bedRoom.makeBed();
+
+        bedRoom.getLamp().turnOn();
     }
 }
--- src/Wall.java
+++ src/Wall.java
@@ -1,52 +1,11 @@
 public class Wall {
-    private double width;
-    private double height;
+    private String direction;
 
-    public Wall() {
+    public Wall(String direction) {
+        this.direction = direction;
     }
 
-    public Wall(double width, double height) {
-
-        if (width < 0) {
-            this.width = 0;
-        }
-        if (width >= 0) {
-            this.width = width;
-        }
-        if (height < 0) {
-            this.height = 0;
-        }
-        if (height >= 0) {
-            this.height = height;
-        }
-    }
-
-    public double getArea() {
-        return this.height * this.width;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        if (width < 0) {
-            this.width = 0;
-        } else {
-            this.width = width;
-        }
-    }
-
-    public double getHeight() {
-
-        return height;
-    }
-
-    public void setHeight(double height) {
-        if (height < 0) {
-            this.height = 0;
-        } else {
-            this.height = height;
-        }
+    public String getDirection() {
+        return direction;
     }
 }
